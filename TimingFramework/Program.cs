8ac50270a6afa70862c115662ec5772b8154e58c
Coombszy <LCCoombs@hotmail.co.uk>
2018-12-10 12:52:16 +0000

    Timer Class Started with method parsing/deligate handling

--- TimingFramework/Program.cs
+++ TimingFramework/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace TimingFramework
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("System Loaded");
+            Timer TestTimer = new Timer();
+            List<object> Data = new List<object>() { "TEST" };
+            TestTimer.SetData(Data);
+            TestTimer.SetMethod(ToParse);
+            TestTimer.DEBUG_run();
+        }
+        public static void ToParse(List<object> data)
+        {
+            if (data.Contains("TEST"))
+            {
+                Console.WriteLine("Matched!");
+            }
         }
     }
 }
---
+++ TimingFramework/Timer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimingFramework
+{
+    public class Timer
+    {
+        //Variables!
+        public delegate void Method(List<object> data);//Deligates!
+        private Method ToCall;//Method to be tested
+        private List<object> DataParsed;//Data to be used with the method
+        private List<int> TestData = new List<int>();
+
+        //Accessors!
+        public void SetMethod(Method MethodToTime)
+        {
+            ToCall = MethodToTime;
+        }
+        public void SetData(List<object> DataToUse)
+        {
+            DataParsed = DataToUse;
+        }
+
+        //Run tests!
+        private bool RunMethod()
+        {
+            if (ToCall != null && DataParsed != null)
+            {
+                ToCall(DataParsed);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("FAILED : Method or Data was not set!");
+                return false; 
+            }
+        }
+
+        //Get/Edit test Data!
+        public List<int> AllTestTimes()
+        {
+            return TestData;
+        }
+        public void WipeAllTestTimes()
+        {
+            TestData = new List<int>();
+        }
+        private void AddTestData(int TimeInSeconds)
+        {
+            TestData.Add(TimeInSeconds);
+        }
+    }
+}
