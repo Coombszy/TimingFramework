6341af4e7e865e43c4b11daa4194a9514979f37b
Coombszy <LCCoombs@hotmail.co.uk>
2018-12-10 11:53:44 +0000

    Project Creation

---
+++ TimingFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimingFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
